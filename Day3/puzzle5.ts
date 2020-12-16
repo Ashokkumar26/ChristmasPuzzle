129d8f2318a3f6f67bda1f492e10e3ddb267ad23
Ashokkumar <koshakkumar3@gmail.com>
2020-12-16 12:11:33 +0530

    adjusted

--- Day3/puzzle5.ts
+++ Day3/puzzle5.ts
@@ -1,16 +1,33 @@
 import { pattern } from "./input";
+
 function Encounter(pattern) {
   let tree = 0,
     squre = 0;
   for (let i = 1; i < pattern.length; i++) {
     let path = pattern[i].split("");
-    let count = 1;
+    let count = 0;
     for (let j = count + 3; j < path.length; j++) {
       path[j] === "." ? squre++ : path[j] === "#" && tree++;
       count += 3;
       break;
     }
   }
-  return [squre, tree];
+  return { squre, tree };
 }
 console.log(Encounter(pattern));
+
+function findTree(pattern) {
+  let j = 3;
+  let tree = 0;
+  let squre = 0;
+  for (let i = 1; i < pattern.length; i++) {
+    if (pattern[i][j] === "#") {
+      tree++;
+    } else {
+      squre++;
+    }
+    j += 3;
+  }
+  return { tree, squre };
+}
+console.log(findTree(pattern));
